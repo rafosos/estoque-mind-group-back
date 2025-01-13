247419a8231275bb16d92ea4e7994dd0086f8df5
rafosos <rafael.lrfrancisco@gmail.com>
2025-01-13 08:55:54 -0300

    add endpoints no index

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 require("dotenv").config()
 const express = require('express');
+const authRouter =  require('./routes/auth')
+const produtoRouter =  require('./routes/produto')
 
 const app = express();
 app.use(express.json());
+app.use('/auth', authRouter);
+app.use('/produto', produtoRouter);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
