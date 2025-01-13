f8798cf106f7b64a93e5246883c1bce593ceb52c
rafosos <rafael.lrfrancisco@gmail.com>
2025-01-13 08:56:07 -0300

    add midddleware

---
+++ middlewares/auth.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+const auth = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    
+    if (!authHeader) {
+        return res.status(401).json({ message: 'Token é obrigatório!' });
+    }
+    
+    const [, token] = authHeader.split(' ');
+    
+    try {
+        const senha = process.env['JWT_PASSWORD'];
+        jwt.verify(token, senha, 
+            function(err, decodedToken) {
+                if(err) return res.status(401).json({message: "Erro com token.", erro: err});
+                else{
+                    res.userId = decodedToken.id
+                }
+            });
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: 'Token Inválido!' });
+    }
+};
+
+module.exports = auth;
