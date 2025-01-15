2a25c50f2d712a0bff4c9caa9e0aea7c8547a297
rafosos <rafael.lrfrancisco@gmail.com>
2025-01-15 15:15:31 -0300

    endpoints movimentacao e script BD

--- routes/produto.js
+++ routes/produto.js
@@ -2,6 +2,7 @@ const express = require('express');
 const Produto = require("../models/produto");
 const authMiddleware = require('../middlewares/auth');
 const multer = require('multer');
+const { Sequelize } = require('sequelize');
 
 const router = express.Router();
 router.use(authMiddleware);
@@ -19,13 +20,16 @@ router.post('/', upload.single('image'), async (req, res) => {
     }
 });
 
-router.patch('/:id/quantidade', async (req, res) => {
-    const {quantidade} = req.body;
-    const {id} = req.params
+router.patch('/movimentacao', async (req, res) => {
+    const {produtos} = req.body;
     
     try{
-        await Produto.update({quantidade}, {where: {id}});
-        res.status(200).json({quantidade})
+        for (let i = 0; i < produtos.length; i++) {
+            const produto = produtos[i];
+            console.log(produto)
+            await Produto.update({quantidade: produto.quantidade}, {where: {id: produto.id}});            
+        }
+        res.status(200).send("Produtos atualizados com sucesso")
     } catch(error){
         res.status(402).json({error})
     }
@@ -47,11 +51,14 @@ router.put('/:id', upload.single('image'), async (req, res) => {
 router.get('/', async(req, res) => {
     try{
         const produtos = await Produto.findAll({where: {userId: req.userId}});
+        throw "asdasd";
         res.status(200).json(produtos);
     }catch(error){
+        console.log('asdasd')
         res.status(402).json({error});
     }
 });
+
 
 router.get('/:id', async(req, res) => {
     const {id} = req.params;
@@ -62,6 +69,30 @@ router.get('/:id', async(req, res) => {
         res.status(402).json({error});
     }
 });
+
+router.get('/filtro/:filtro', async(req,res) => {
+    const {filtro} = req.params;
+    let {idsEscolhidos} = req.query;
+    let where = {
+        userId: req.userId,
+        nome: {[Sequelize.Op.like]: `%${filtro}%`},
+    }
+
+    if(idsEscolhidos){
+        where["id"] = {[Sequelize.Op.notIn]: typeof idsEscolhidos == 'string' ? [idsEscolhidos] : idsEscolhidos}
+    }
+
+    try{
+        const produtos = await Produto.findAll({
+            attributes: ['id',  'nome', 'imagem', 'valor', 'quantidade'],
+            where
+        });
+        res.status(200).json(produtos);
+    }catch(error){
+        console.log(error)
+        res.status(402).json({error});
+    }
+})
 
 router.delete('/:id', async(req, res) => {
     const {id} = req.params;
